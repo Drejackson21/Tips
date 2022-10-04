b21618accd618f5cad47fba88465058bee2fc95a
dandrejackson116@yahoo.com <dandrejackson116@yahoo.com>
2022-10-03 17:31:20 -0700

    used querySelector to create dom elements

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputEl = document.querySelector("#nill-amount");
+let buttonEl = document.querySelector("#calculate-tip");
+let tipAmount = document.querySelector("#tip-amount");
+
+buttonEl.addEventListener("click", );
